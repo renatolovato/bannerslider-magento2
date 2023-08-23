ed723fdc8e2b0143cd4699f71354c694ea66b672
Renato Lovato <renatolovato@gmail.com>
2023-08-23 15:33:37 -0400

    Fixing Admin Banner Grid

---
+++ Block/Adminhtml/Banner/Grid.php
@@ -0,0 +1,264 @@
+<?php
+
+/**
+ * Magestore
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magestore.com license that is
+ * available through the world-wide-web at this URL:
+ * http://www.magestore.com/license-agreement.html
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Magestore
+ * @package     Magestore_Bannerslider
+ * @copyright   Copyright (c) 2012 Magestore (http://www.magestore.com/)
+ * @license     http://www.magestore.com/license-agreement.html
+ */
+
+namespace Magestore\Bannerslider\Block\Adminhtml\Banner;
+
+use Magestore\Bannerslider\Model\Status;
+
+/**
+ * Slider grid.
+ * @category Magestore
+ * @package  Magestore_Bannerslider
+ * @module   Bannerslider
+ * @author   Magestore Developer
+ */
+class Grid extends \Magento\Backend\Block\Widget\Grid\Extended
+{
+    /**
+     * slider collection factory.
+     *
+     * @var \Magestore\Bannerslider\Model\ResourceModel\Banner\CollectionFactory
+     */
+    protected $_bannerCollectionFactory;
+
+    /**
+     * helper.
+     *
+     * @var \Magestore\Bannerslider\Helper\Data
+     */
+    protected $_bannersliderHelper;
+
+    /**
+     * [__construct description].
+     *
+     * @param \Magento\Backend\Block\Template\Context                         $context
+     * @param \Magento\Backend\Helper\Data                                    $backendHelper
+     * @param \Magestore\Bannerslider\Model\ResourceModel\Slider\CollectionFactory $sliderCollectionFactory
+     * @param \Magestore\Bannerslider\Helper\Data                             $bannersliderHelper
+     * @param array                                                           $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Backend\Helper\Data $backendHelper,
+        \Magestore\Bannerslider\Model\ResourceModel\Banner\CollectionFactory $bannerCollectionFactory,
+        \Magestore\Bannerslider\Helper\Data $bannersliderHelper,
+        array $data = []
+    ) {
+        $this->_bannerCollectionFactory = $bannerCollectionFactory;
+        $this->_bannersliderHelper = $bannersliderHelper;
+        parent::__construct($context, $backendHelper, $data);
+    }
+
+    /**
+     * [_construct description].
+     *
+     * @return [type] [description]
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setId('bannerGrid');
+        $this->setDefaultSort('banner_id');
+        $this->setDefaultDir('ASC');
+        $this->setSaveParametersInSession(true);
+        $this->setUseAjax(true);
+    }
+
+    /**
+     * prepare collection.
+     *
+     * @return [type] [description]
+     */
+    protected function _prepareCollection()
+    {
+        $collection = $this->_bannerCollectionFactory->create();
+        $this->setCollection($collection);
+
+        return parent::_prepareCollection();
+    }
+
+    /**
+     * @return $this
+     */
+    protected function _prepareColumns()
+    {
+        $this->addColumn(
+            'banner_id',
+            [
+                'header' => __('Banner ID'),
+                'type' => 'number',
+                'index' => 'banner_id',
+                'header_css_class' => 'col-id',
+                'column_css_class' => 'col-id',
+            ]
+        );
+        $this->addColumn(
+            'name',
+            [
+                'header' => __('Name'),
+                'index' => 'name',
+                'class' => 'xxx',
+                'width' => '50px',
+            ]
+        );
+        $this->addColumn(
+            'image',
+            [
+                'header' => __('Image'),
+                'filter' => false,
+                'class' => 'xxx',
+                'width' => '50px',
+                'renderer' => 'Magestore\Bannerslider\Block\Adminhtml\Banner\Helper\Renderer\Image',
+            ]
+        );
+        $this->addColumn(
+            'click_url',
+            [
+                'header' => __('Url'),
+                'index' => 'click_url'
+            ]
+        );
+        $this->addColumn(
+            'start_time',
+            [
+                'header' => __('Starting time'),
+                'type' => 'datetime',
+                'index' => 'start_time',
+                'class' => 'xxx',
+                'width' => '50px',
+                'timezone' => true,
+            ]
+        );
+
+        $this->addColumn(
+            'end_time',
+            [
+                'header' => __('Ending time'),
+                'type' => 'datetime',
+                'index' => 'end_time',
+                'class' => 'xxx',
+                'width' => '50px',
+                'timezone' => true,
+            ]
+        );
+
+        $this->addColumn(
+            'status',
+            [
+                'header' => __('Status'),
+                'index' => 'status',
+                'type' => 'options',
+                'filter_index' => 'main_table.status',
+                'options' => Status::getAvailableStatuses(),
+            ]
+        );
+
+        $this->addColumn(
+            'edit',
+            [
+                'header' => __('Edit'),
+                'type' => 'action',
+                'getter' => 'getId',
+                'actions' => [
+                    [
+                        'caption' => __('Edit'),
+                        'url' => [
+                            'base' => '*/*/edit',
+                        ],
+                        'field' => 'banner_id',
+                    ],
+                ],
+                'filter' => false,
+                'sortable' => false,
+                'index' => 'stores',
+                'header_css_class' => 'col-action',
+                'column_css_class' => 'col-action',
+            ]
+        );
+        $this->addExportType('*/*/exportCsv', __('CSV'));
+        $this->addExportType('*/*/exportXml', __('XML'));
+        $this->addExportType('*/*/exportExcel', __('Excel'));
+
+        return parent::_prepareColumns();
+    }
+
+    /**
+     * @return $this
+     */
+    protected function _prepareMassaction()
+    {
+        $this->setMassactionIdField('banner_id');
+        $this->getMassactionBlock()->setFormFieldName('banner');
+
+        $this->getMassactionBlock()->addItem(
+            'delete',
+            [
+                'label' => __('Delete'),
+                'url' => $this->getUrl('bannerslideradmin/*/massDelete'),
+                'confirm' => __('Are you sure?'),
+            ]
+        );
+
+        $statuses = Status::getAvailableStatuses();
+
+        array_unshift($statuses, ['label' => '', 'value' => '']);
+        $this->getMassactionBlock()->addItem(
+            'status',
+            [
+                'label' => __('Change status'),
+                'url' => $this->getUrl('bannerslideradmin/*/massStatus', ['_current' => true]),
+                'additional' => [
+                    'visibility' => [
+                        'name' => 'status',
+                        'type' => 'select',
+                        'class' => 'required-entry',
+                        'label' => __('Status'),
+                        'values' => $statuses,
+                    ],
+                ],
+            ]
+        );
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGridUrl()
+    {
+        return $this->getUrl('*/*/grid', array('_current' => true));
+    }
+
+    /**
+     * get row url
+     * @param  object $row
+     * @return string
+     */
+    public function getRowUrl($row)
+    {
+        return $this->getUrl(
+            '*/*/edit',
+            array('banner_id' => $row->getId())
+        );
+    }
+}
